5098cbbc57b173a32e764c41d9936086fb93f8f2
osocron <osocron@gmail.com>
2016-12-08 16:00:52 -0600

    Barcode done like a boss

--- app/src/main/java/fei/mx/uv/ofertasmas/CuponActivity.kt
+++ app/src/main/java/fei/mx/uv/ofertasmas/CuponActivity.kt
@@ -2,12 +2,20 @@ package fei.mx.uv.ofertasmas
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.bottlerocketstudios.barcode.generation.ui.BarcodeView
 
 class CuponActivity : AppCompatActivity() {
+
+    val barCodeView by lazy { findViewById(R.id.generation_barcode_image) as BarcodeView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cupon)
+        getCuponCode()
+    }
+
+    private fun getCuponCode() {
+        barCodeView.setBarcodeText(intent.getIntExtra("codigoCupon", 1).toString())
     }
 
 }
